7d1cc2ef2d166f32f049d8aa53ccb7edf0211101
Yuma Satake <satous.y@icloud.com>
2024-05-31 10:36:17 +0900

    feat: change-auth-callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useMemo, useState } from "react";
 import OauthPopup from "react-oauth-popup";
 import "./global.css";
 
@@ -11,7 +11,7 @@ const SCOPES =
 const params = {
   client_id: CLIENT_ID,
   redirect_uri: REDIRECT_URI,
-  response_type: "token",
+  response_type: "code",
   scope: SCOPES,
   include_granted_scopes: "true",
   state: "pass-through value",
@@ -23,44 +23,24 @@ const AUTH_URL = authUrl.toString();
 const App: FC = () => {
   const [code, setCode] = useState("");
 
-  const currentUrl = window.location.href;
-  const urlParams = (() => {
-    if (!currentUrl.includes("?")) return {};
-    //現在のURLに付与されているパラメータを配列として取得
-    const urlParam = currentUrl.split("?");
-    const param = urlParam[1].split("&");
-    const paramArray: { [key: string]: string } = {};
-    for (const element of param) {
-      const paramItem = element.split("=");
-      paramArray[paramItem[0]] = paramItem[1];
-    }
-    return paramArray;
-  })();
-
   const handleCode = (code: string): void => {
     setCode(code);
   };
 
+  const isCodeEmpty = useMemo(() => code === "", [code]);
+
   return (
-    <div>
-      <h3>OAuthCode：{code === "" ? "empty" : code}</h3>
-      <p>
-        <h4>AuthURL</h4>
-        <br />
-        {AUTH_URL}
-      </p>
-      <div>
-        <h4>Params</h4>
-        <br />
-        {Object.keys(urlParams).map((key) => {
-          const typedKey = key as keyof typeof urlParams;
-          return (
-            <p key={key}>
-              {key}: {urlParams[typedKey]}
-            </p>
-          );
-        })}
-      </div>
+    <div style={{ padding: 30 }}>
+      <h3>
+        [OAuthCode]
+        <p
+          style={{
+            color: isCodeEmpty ? "red" : "green",
+          }}
+        >
+          {isCodeEmpty ? "EMPTY" : code}
+        </p>
+      </h3>
       <OauthPopup
         title='Login with Google'
         width={600}
